a37e1e5fd19ffeb3a41b68c2d47fadbaa508cd5b
VictorGuevara <VEMlone@hotmail.com>
2025-06-25 11:43:59 -0600

    Actualización 25-06.2025 - nuevo diseño

--- js/principal.js
+++ js/principal.js
@@ -1,62 +1,32 @@
-/* --------------------------------------------------------------------------------- */
-/*                                                                                   */
-/*                                    VARIABLES                                      */
-/*                                                                                   */
-/* --------------------------------------------------------------------------------- */
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
-// Variable del link para llamar los colores de la página.
-const tag_link_color_page = document.getElementById('tag_link_color_page');
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-// MENU MOSTRAR Y OCULTAR 
-const navMenu = document.getElementById('nav-menu');
-const toggleMenu = document.getElementById('nav-toggle');
-const closeMenu = document.getElementById('nav-close');
-let navLink = document.querySelectorAll('.nav__link');
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute("id");
 
-/* --------------------------------------------------------------------------------- */
-/*                                                                                   */
-/*                                    FUNCIONES                                      */
-/*                                                                                   */
-/* --------------------------------------------------------------------------------- */
-
-/* --------------------------------------------------------------------------------- */
-/*                      Función que cambia los colores del tema                      */
-/* --------------------------------------------------------------------------------- */
-
-const change_color_theme = (nombre_docCSS) => {
-	tag_link_color_page.href = '';
-	tag_link_color_page.href = './css/' + nombre_docCSS + '.css';
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
 }
+window.addEventListener("scroll", scrollActive);
 
-// Función declarativa para llamar los cambios de los items.
-function linkAction() {
-    /*Active link*/
-    navLink.forEach(n => n.classList.remove('active'));
-    this.classList.add('active');
-
-    /*Remove menu mobile*/
-    navMenu.classList.remove('show')
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
 }
-
-
-/* --------------------------------------------------------------------------------- */
-/*                                                                                   */
-/*                                     EVENTOS                                       */
-/*                                                                                   */
-/* --------------------------------------------------------------------------------- */
-
-document.addEventListener('DOMContentLoaded', () =>	{
-	change_color_theme('colores');
-});
-
-// Mostar menú 
-toggleMenu.addEventListener('click', ()=>{
-    navMenu.classList.toggle('show');
-})
-
-// Ocultar menú
-closeMenu.addEventListener('click', ()=>{
-    navMenu.classList.remove('show');
-})
-
-navLink.forEach(n => n.addEventListener('click', linkAction));
+window.addEventListener("scroll", scrollHeader);
